2e1d76f583a4505317a2edad6f9002b5d9a7fef6
raees <mohitdamke1@gmail.com>
2024-09-02 16:05:17 +0530

    Done with add story

---
+++ app/src/main/java/com/example/friendzone/dimension/FontDim.kt
@@ -0,0 +1,12 @@
+package com.example.friendzone.dimension
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.friendzone.R
+
+object FontDim {
+
+    val Medium = FontFamily(Font(R.font.hanken_medium))
+    val Bold = FontFamily(Font(R.font.hanken_bold))
+    val SemiBold = FontFamily(Font(R.font.hanken_semibold))
+}
---
+++ app/src/main/java/com/example/friendzone/dimension/TextDim.kt
@@ -0,0 +1,10 @@
+package com.example.friendzone.dimension
+
+import androidx.compose.ui.unit.sp
+
+object TextDim {
+    val titleTextSize = 18.sp
+    val bodyTextSize = 16.sp
+    val secondaryTextSize = 14.sp
+    val tertiaryTextSize = 12.sp
+}
